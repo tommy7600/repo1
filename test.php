1c5b63201f86391c588d63f895bcc3ebda496718
Tomasz Lorenc <tlorenc@future-processing.com>
2012-11-29 11:22:25 +0100

    test

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+$cs = 0;
+/**
+ * Created by JetBrains PhpStorm.
+ * User: tlorenc
+ * Date: 11/29/12
+ * Time: 11:19 AM
+ * To change this template use File | Settings | File Templates.
+ */
